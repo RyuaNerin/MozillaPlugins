fec20d436a567b7be157ffae4f2274b81916dd9a
RyuaNerin <github@ryuanerin.kr>
2014-03-10 02:09:51 +0900

    커밋 생성

---
+++ frmMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MozillaPlugins
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다.
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lsvList = new System.Windows.Forms.ListView();
+			this.lsvList0 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.lsvList1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.lsvList2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.lsvList3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.lsvList4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.bgwLoad = new System.ComponentModel.BackgroundWorker();
+			this.bgwDelete = new System.ComponentModel.BackgroundWorker();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lsvList
+			// 
+			this.lsvList.CheckBoxes = true;
+			this.lsvList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lsvList0,
+            this.lsvList1,
+            this.lsvList2,
+            this.lsvList3,
+            this.lsvList4});
+			this.lsvList.FullRowSelect = true;
+			this.lsvList.GridLines = true;
+			this.lsvList.Location = new System.Drawing.Point(12, 12);
+			this.lsvList.MultiSelect = false;
+			this.lsvList.Name = "lsvList";
+			this.lsvList.ShowGroups = false;
+			this.lsvList.Size = new System.Drawing.Size(427, 189);
+			this.lsvList.TabIndex = 0;
+			this.lsvList.UseCompatibleStateImageBehavior = false;
+			this.lsvList.View = System.Windows.Forms.View.Details;
+			// 
+			// lsvList0
+			// 
+			this.lsvList0.Text = "KeyName";
+			this.lsvList0.Width = 170;
+			// 
+			// lsvList1
+			// 
+			this.lsvList1.Text = "ProductName";
+			this.lsvList1.Width = 160;
+			// 
+			// lsvList2
+			// 
+			this.lsvList2.Text = "Description";
+			this.lsvList2.Width = 250;
+			// 
+			// lsvList3
+			// 
+			this.lsvList3.Text = "Path";
+			this.lsvList3.Width = 250;
+			// 
+			// lsvList4
+			// 
+			this.lsvList4.Text = "Registry";
+			this.lsvList4.Width = 200;
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Enabled = false;
+			this.btnDelete.Location = new System.Drawing.Point(333, 207);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(106, 35);
+			this.btnDelete.TabIndex = 1;
+			this.btnDelete.Text = "삭제";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+			// 
+			// bgwLoad
+			// 
+			this.bgwLoad.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwLoad_DoWork);
+			this.bgwLoad.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwLoad_RunWorkerCompleted);
+			// 
+			// bgwDelete
+			// 
+			this.bgwDelete.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwDelete_DoWork);
+			this.bgwDelete.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwDelete_RunWorkerCompleted);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label1.ForeColor = System.Drawing.Color.Blue;
+			this.label1.Location = new System.Drawing.Point(12, 204);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(106, 15);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "파랑 : 삭제 비추천";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label2.ForeColor = System.Drawing.Color.Red;
+			this.label2.Location = new System.Drawing.Point(12, 219);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(94, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "빨강 : 삭제 추천\r\n";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(451, 254);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.lsvList);
+			this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "MozillaPlugins";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.Resize += new System.EventHandler(this.frmMain_Resize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView lsvList;
+		private System.Windows.Forms.ColumnHeader lsvList0;
+		private System.Windows.Forms.ColumnHeader lsvList1;
+		private System.Windows.Forms.ColumnHeader lsvList2;
+		private System.Windows.Forms.ColumnHeader lsvList3;
+		private System.Windows.Forms.ColumnHeader lsvList4;
+		private System.Windows.Forms.Button btnDelete;
+		private System.ComponentModel.BackgroundWorker bgwLoad;
+		private System.ComponentModel.BackgroundWorker bgwDelete;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+
+	}
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace MozillaPlugins
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			this.bgwLoad.RunWorkerAsync();
+		}
+
+		private void frmMain_Resize(object sender, EventArgs e)
+		{
+			this.lsvList.Width = this.Width - 40;
+			this.lsvList.Height = this.Height - 103;
+
+			this.btnDelete.Left = this.Width - 134;
+			this.btnDelete.Top = this.Height - 85;
+
+			this.label1.Top = this.Height - 88;
+			this.label2.Top = this.Height - 73;
+		}
+
+		private string[] WebLoad(WebClient wc, string url)
+		{
+			try
+			{
+				string body = wc.DownloadString(url);
+				body = body.Replace("\r", "\n");
+				while (body.IndexOf("\n\n") >= 0)
+					body = body.Replace("\n\n", "\n");
+
+				return body.Split('\n');
+			}
+			catch
+			{
+				return new string[0];
+			}
+		}
+
+		private delegate void dv();
+		private void bgwLoad_DoWork(object sender, DoWorkEventArgs e)
+		{
+			string[] safe, rocommend;
+
+			using (WebClient wc = new WebClient())
+			{
+				wc.Encoding = Encoding.UTF8;
+				rocommend = WebLoad(wc, "https://raw.github.com/RyuaNerin/MozillaPlugins/master/recom.txt");
+				safe = WebLoad(wc, "https://raw.github.com/RyuaNerin/MozillaPlugins/master/safe.txt");
+			}
+
+			List<RegistryKey> lst = new List<RegistryKey>();
+
+			RegistryKey r;
+
+			r = Registry.CurrentUser
+				.CreateSubKey("SOFTWARE")
+				.CreateSubKey("MozillaPlugins");
+
+			object o = r.GetValue("@");
+
+			r = Registry.CurrentUser
+				.CreateSubKey("SOFTWARE")
+				.CreateSubKey("MozillaPlugins");
+
+			foreach (string s in r.GetSubKeyNames())
+				lst.Add(r.CreateSubKey(s));
+
+			r = Registry.LocalMachine
+				.CreateSubKey("SOFTWARE")
+				.CreateSubKey("MozillaPlugins");
+
+			foreach (string s in r.GetSubKeyNames())
+				lst.Add(r.CreateSubKey(s));
+
+			try
+			{
+				r = Registry.LocalMachine
+					.CreateSubKey("SOFTWARE")
+					.CreateSubKey("Wow6432Node")
+					.CreateSubKey("MozillaPlugins");
+
+				foreach (string s in r.GetSubKeyNames())
+					lst.Add(r.CreateSubKey(s));
+			}
+			catch { }
+
+			this.Invoke(new dv(delegate() { this.lsvList.Items.Clear(); }));
+
+			foreach (RegistryKey rk in lst)
+			{
+				ListViewItem item = new ListViewItem();
+				item.Text = rk.Name.Substring(rk.Name.LastIndexOf('\\') + 1);
+				item.SubItems.Add((string)rk.GetValue("ProductName"));
+				item.SubItems.Add((string)rk.GetValue("Description"));
+				item.SubItems.Add((string)rk.GetValue("Path"));
+				item.SubItems.Add(rk.Name);
+				item.Tag = rk;
+
+				if ((string)rk.GetValue("Path") == "disabled")
+				{
+					item.ForeColor = Color.Gray;
+				}
+				else
+				{
+				if (!File.Exists((string)rk.GetValue("Path")))
+					item.ForeColor = Color.Red;
+
+				foreach (string s in rocommend)
+				{
+					if (item.Text.StartsWith(s) || item.Text.StartsWith("@" + s))
+					{
+						item.ForeColor = Color.Red;
+						break;
+					}
+				}
+
+				foreach (string s in safe)
+				{
+					if (item.Text.StartsWith(s) || item.Text.StartsWith("@" + s))
+					{
+						item.ForeColor = Color.Blue;
+						break;
+					}
+				}
+				}
+				
+
+				this.Invoke(new dv(delegate() { this.lsvList.Items.Add(item); }));
+			}
+		}
+
+		private void bgwLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			this.btnDelete.Enabled = true;
+		}
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show(this, "삭제하시겠습니까?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk)
+				!= DialogResult.Yes)
+				return;
+
+			this.btnDelete.Enabled = false;
+			this.bgwDelete.RunWorkerAsync();
+		}
+
+		private delegate int d_i();
+		private delegate RegistryKey d_r();
+		private void bgwDelete_DoWork(object sender, DoWorkEventArgs e)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append("Windows Registry Editor Version 5.00\r\n\r\n");
+
+			int CheckedCount = (int)this.Invoke(new d_i(delegate() { return this.lsvList.CheckedIndices.Count; }));
+
+			for (int i = 0; i < CheckedCount; i++)
+			{
+				RegistryKey rk = (RegistryKey)this.Invoke(new d_r(delegate() { return (RegistryKey)this.lsvList.CheckedItems[i].Tag; }));
+				RegBackup(rk, sb);
+				rk.DeleteSubKeyTree("");
+				rk.Close();
+			}
+
+			File.WriteAllText(DateTime.Now.ToString("백업 yyyy-MM-dd HH-mm-ss") + ".reg", sb.ToString(), Encoding.ASCII);
+		}
+		private void RegBackup(RegistryKey key, StringBuilder sb)
+		{
+			sb.Append("\r\n[");
+			sb.Append(key.Name);
+			sb.Append("]\r\n");
+
+			foreach (string sKey in key.GetValueNames())
+			{
+				RegistryValueKind rvk = key.GetValueKind(sKey);
+
+				switch (rvk)
+				{
+					case RegistryValueKind.String:
+						sb.AppendFormat("\"{0}\"=\"{1}\"\r\n", sKey, (string)key.GetValue(sKey));
+						break;
+
+					case RegistryValueKind.DWord:
+						sb.AppendFormat("\"{0}\"=dword:{1:X}\r\n", sKey, (int)key.GetValue(sKey));
+						break;
+
+					case RegistryValueKind.QWord:
+						sb.AppendFormat("\"{0}\"=dword:{1}\r\n", sKey, StringFromQWord((long)key.GetValue(sKey)));
+						break;
+
+					case RegistryValueKind.Binary:
+						sb.AppendFormat("\"{0}\"=hex:{1}\r\n", sKey, BinarytoString((byte[])key.GetValue(sKey)));
+						break;
+
+					case RegistryValueKind.MultiString:
+						sb.AppendFormat("\"{0}\"=hex:{1}\r\n", sKey, stringFormMultiSz((string[])key.GetValue(sKey)));
+						break;
+
+					case RegistryValueKind.ExpandString:
+						sb.AppendFormat("\"{0}\"=hex(2):{1}\r\n", sKey, stringFromExpandSz((string)key.GetValue(sKey)));
+						break;
+				}
+			}
+
+			foreach (string sKey in key.GetSubKeyNames())
+				this.RegBackup(key.OpenSubKey(sKey), sb);
+		}
+		private string BinarytoString(byte[] arr)
+		{
+			StringBuilder sb = new StringBuilder(arr.Length * 3);
+			foreach (byte b in arr)
+				if (b < 16)
+					sb.AppendFormat("0{0:X},", b);
+				else
+					sb.AppendFormat("{0:X},", b);
+
+			sb.Remove(sb.Length - 1, 1);
+			return sb.ToString();
+		}
+		private string StringFromQWord(long dat)
+		{
+			// 0x0123456789ABCDEF
+			// FE DC BA 98 76 54 32 01
+			//  0  1  2  3  4  5  6  7
+
+			StringBuilder sb = new StringBuilder(24);
+
+			for (int i = 0; i < 8; i++)
+			{
+				byte b = (byte)((dat >> (i * 8)) & 0x100);
+				if (( i == 7 | 0 < b) && b < 16)
+					sb.AppendFormat("0{0:X},", b);
+				else if (16 < b)
+					sb.AppendFormat("{0:X},", b);
+			}
+
+			sb.Remove(sb.Length - 1, 1);
+			return sb.ToString();
+		}
+		private string stringFromExpandSz(string str)
+		{
+			return BinarytoString(Encoding.Unicode.GetBytes(str + "\x0"));
+		}
+		private string stringFormMultiSz(string[] str)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (string s in str)
+				sb.Append(stringFromExpandSz(s) + ",");
+
+			sb.Append("00,00");
+			return sb.ToString();
+		}
+
+		private void bgwDelete_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			MessageBox.Show(this, "삭제되었습니다!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+			this.btnDelete.Enabled = false;
+			this.bgwLoad.RunWorkerAsync();
+		}
+	}
+}
